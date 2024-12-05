eb13d6ce64875fba45da4b476ecae70f8495d5f8
gonchar <s@gonchar.me>
2024-12-05 18:55:22 +0000

    azure factory

--- Sources/OpenAI/Public/Service/OpenAIServiceFactory.swift
+++ Sources/OpenAI/Public/Service/OpenAIServiceFactory.swift
@@ -8,63 +8,89 @@
 import Foundation
 
 public class OpenAIServiceFactory {
-   
-   // MARK: OpenAI
-   
-   /// Creates and returns an instance of `OpenAIService`.
-   ///
-   /// - Parameters:
-   ///   - apiKey: The API key required for authentication.
-   ///   - organizationID: The optional organization ID for multi-tenancy (default is `nil`).
-   ///   - configuration: The URL session configuration to be used for network calls (default is `.default`).
-   ///   - decoder: The JSON decoder to be used for parsing API responses (default is `JSONDecoder.init()`).
-   ///   - debugEnabled: If `true` service prints event on DEBUG builds, default to `false`.
-
-   /// - Returns: A fully configured object conforming to `OpenAIService`.
-   public static func service(
-      apiKey: String,
-      organizationID: String? = nil,
-      configuration: URLSessionConfiguration = .default,
-      decoder: JSONDecoder = .init(),
-      debugEnabled: Bool = false)
-      -> OpenAIService
-   {
-      DefaultOpenAIService(
-         apiKey: apiKey,
-         organizationID: organizationID,
-         configuration: configuration,
-         decoder: decoder,
-         debugEnabled: debugEnabled)
-   }
-   
-   
-   // MARK: Proxy Path
-
-   /// Creates and returns an instance of `OpenAIService`.
-   ///
-   /// Use this service if you need to provide a custom URL with a proxy path, for example to run Groq.
-   ///
-   /// - Parameters:
-   ///   - apiKey: The optional API key required for authentication.
-   ///   - baseURL: The local host URL.  e.g "https://api.groq.com" or "https://generativelanguage.googleapis.com"
-   ///   - proxyPath: The proxy path e.g `openai`
-   ///   - overrideVersion: The API version. defaults to `V1`
-   ///   - debugEnabled: If `true` service prints event on DEBUG builds, default to `false`.
-   ///
-   /// - Returns: A fully configured object conforming to `OpenAIService`.
-   public static func service(
-      apiKey: String,
-      overrideBaseURL: String,
-      proxyPath: String? = nil,
-      overrideVersion: String? = nil,
-      debugEnabled: Bool = false)
-      -> OpenAIService
-   {
-      DefaultOpenAIService(
-         apiKey: apiKey,
-         baseURL: overrideBaseURL,
-         proxyPath: proxyPath,
-         overrideVersion: overrideVersion,
-         debugEnabled: debugEnabled)
-   }
+  
+  // MARK: OpenAI
+  
+  /// Creates and returns an instance of `OpenAIService`.
+  ///
+  /// - Parameters:
+  ///   - apiKey: The API key required for authentication.
+  ///   - organizationID: The optional organization ID for multi-tenancy (default is `nil`).
+  ///   - configuration: The URL session configuration to be used for network calls (default is `.default`).
+  ///   - decoder: The JSON decoder to be used for parsing API responses (default is `JSONDecoder.init()`).
+  ///   - debugEnabled: If `true` service prints event on DEBUG builds, default to `false`.
+  
+  /// - Returns: A fully configured object conforming to `OpenAIService`.
+  public static func service(
+    apiKey: String,
+    organizationID: String? = nil,
+    configuration: URLSessionConfiguration = .default,
+    decoder: JSONDecoder = .init(),
+    debugEnabled: Bool = false)
+  -> OpenAIService
+  {
+    DefaultOpenAIService(
+      apiKey: apiKey,
+      organizationID: organizationID,
+      configuration: configuration,
+      decoder: decoder,
+      debugEnabled: debugEnabled)
+  }
+  
+  
+  // MARK: Azure
+  
+  /// Creates and returns an instance of `OpenAIService`.
+  ///
+  /// - Parameters:
+  ///   - azureConfiguration: The AzureOpenAIConfiguration.
+  ///   - urlSessionConfiguration: The URL session configuration to be used for network calls (default is `.default`).
+  ///   - decoder: The JSON decoder to be used for parsing API responses (default is `JSONDecoder.init()`).
+  ///   - debugEnabled: If `true` service prints event on DEBUG builds, default to `false`.
+  ///
+  /// - Returns: A fully configured object conforming to `OpenAIService`.
+  public static func service(
+    azureConfiguration: AzureOpenAIConfiguration,
+    urlSessionConfiguration: URLSessionConfiguration = .default,
+    decoder: JSONDecoder = .init(),
+    debugEnabled: Bool = false)
+  -> OpenAIService
+  {
+    DefaultOpenAIAzureService(
+      azureConfiguration: azureConfiguration,
+      urlSessionConfiguration: urlSessionConfiguration,
+      decoder: decoder,
+      debugEnabled: debugEnabled)
+  }
+  
+  
+  // MARK: Proxy Path
+  
+  /// Creates and returns an instance of `OpenAIService`.
+  ///
+  /// Use this service if you need to provide a custom URL with a proxy path, for example to run Groq.
+  ///
+  /// - Parameters:
+  ///   - apiKey: The optional API key required for authentication.
+  ///   - baseURL: The local host URL.  e.g "https://api.groq.com" or "https://generativelanguage.googleapis.com"
+  ///   - proxyPath: The proxy path e.g `openai`
+  ///   - overrideVersion: The API version. defaults to `V1`
+  ///   - debugEnabled: If `true` service prints event on DEBUG builds, default to `false`.
+  ///
+  /// - Returns: A fully configured object conforming to `OpenAIService`.
+  public static func service(
+    apiKey: String,
+    overrideBaseURL: String,
+    proxyPath: String? = nil,
+    overrideVersion: String? = nil,
+    debugEnabled: Bool = false)
+  -> OpenAIService
+  {
+    DefaultOpenAIService(
+      apiKey: apiKey,
+      baseURL: overrideBaseURL,
+      proxyPath: proxyPath,
+      overrideVersion: overrideVersion,
+      debugEnabled: debugEnabled)
+  }
 }
